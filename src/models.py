0fc6ff2801158096cf40e911864912accc6a17a1
jvalbuena0317 <146770952+jvalbuena0317@users.noreply.github.com>
2024-02-06 19:48:34 +0000

    updated

--- src/models.py
+++ src/models.py
@@ -1,32 +1,56 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    password = Column (String, nullable = False)
+    subscription_date= Column (DateTime, nullable = False)
+    favorite = relationship ('favorite')
+                               
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    planet_name = Column(String(250))
+    planet_description = Column(String(250))
+    image_url = Column (String (150), nullable = True)
+    favorite = relationship ('favorite')
 
-    def to_dict(self):
-        return {}
+class Favorite(Base):
+     __tablename__ = 'favorite'
+     id = Column(Integer, primary_key=True)
+     planet_id = Column (Integer, ForeignKey('planet.id'), nullable= True)
+     character_id = Column (Integer, ForeignKey('character.id'), nullable= True)
+     user_id = Column (Integer, ForeignKey('user.id'), nullable= False)
+     vehicle_id = Column (Integer, ForeignKey('vehicle.id'), nullable= True)
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    caracther_name = Column(String(250))
+    caracther_description = Column(String(250))
+    image_url = Column (String (150), nullable = True)
+    favorite= relationship ('favorite')
+
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    vehicle_name = Column(String(250))
+    vehicle_description = Column(String(250))
+    image_url = Column (String (150), nullable = True)
+    favorite= relationship ('favorite')
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
